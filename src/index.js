9aaee869fb1ec977d8c1839099a34e606386c90b
emanuelbalcazar <emanuelbalcazar13@gmail.com>
2018-06-09 19:48:37 -0300

    Probando si funciona travis

--- src/index.js
+++ src/index.js
@@ -5,3 +5,4 @@ console.log('Sumando:', calculator.sum(3, 5));
 console.log('Restando:', calculator.sub(10, 3));
 console.log('Multiplicando:', calculator.mul(3, 6));
 console.log('Dividiendo:', calculator.div(10, 2));
+
